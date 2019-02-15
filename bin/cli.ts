f77895d2cb98c920d3e9dd4bd3123daba4dffb8d
rhysd <lin90162@yahoo.co.jp>
2019-02-15 15:10:00 +0900

    detach application process from shell when running it from command line

--- bin/cli.ts
+++ bin/cli.ts
@@ -6,8 +6,8 @@ import commander = require('commander');
 
 const AfterTweetActions: ConfigAfterTweet[] = ['new tweet', 'reply previous', 'close', 'quit'];
 
-const { hashtags, args, afterTweet } = commander
-    .version('0.0.2')
+const { hashtags, args, afterTweet, detach } = commander
+    .version('0.0.3')
     .usage('[options] [text]')
     .option('-t --hashtags <list>', 'Comma-separated list of hashtags', s => s.split(','))
     .option(
@@ -15,6 +15,7 @@ const { hashtags, args, afterTweet } = commander
         'What to do after sending tweet. One of ' + AfterTweetActions.map(a => `'${a}'`).join(', '),
         (s: string) => s.toLowerCase(),
     )
+    .option('--no-detach', '(Do not) Detach process from shell', false)
     .parse(process.argv);
 
 // Verify --after-tweet
@@ -36,6 +37,14 @@ const electron = require('electron') as any;
 // First argument is path to `node`, second argument is path to script
 const argv = [join(__dirname, '..'), '--', JSON.stringify(opts)];
 
-cp.spawn(electron, argv, {
-    stdio: 'inherit',
-});
+if (detach) {
+    // TODO?: Output stderr to $DATA_DIR/log.txt
+    cp.spawn(electron, argv, {
+        stdio: 'ignore',
+        detached: true,
+    }).unref();
+} else {
+    cp.spawn(electron, argv, {
+        stdio: 'inherit',
+    });
+}
