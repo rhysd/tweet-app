e84c4b8bce4263bb1f641e31b397ae4629870cc7
rhysd <lin90162@yahoo.co.jp>
2023-04-07 20:42:15 +0900

    0.4.2

--- bin/cli.ts
+++ bin/cli.ts
@@ -63,7 +63,7 @@ const AfterTweetActions: ConfigAfterTweet[] = ['new tweet', 'reply previous', 'c
 
 const parsed = program
     .name('tweet')
-    .version('0.4.1')
+    .version('0.4.2')
     .usage('[options] [text]')
     .description('Desktop application for tweeting. Timeline never shows up.')
     .option('-t --hashtags <list>', 'comma-separated list of hashtags (e.g. "js,react,node")', s =>
